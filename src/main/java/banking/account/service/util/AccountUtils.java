e4b30e6c3ece2d666effe8dcc70c7bc2f4be8487
Manuel Pache <manuel.pache@europace.de>
2022-03-23 19:43:09 +0100

    [MPA] Fixed Currency localization

--- src/main/java/banking/account/service/util/AccountUtils.java
+++ src/main/java/banking/account/service/util/AccountUtils.java
@@ -2,12 +2,13 @@ package banking.account.service.util;
 
 import java.math.BigDecimal;
 import java.text.NumberFormat;
+import java.util.Locale;
 
 public class AccountUtils {
 
   public static String formatBalanceForOutput(BigDecimal value) {
     if (value != null) {
-      return NumberFormat.getCurrencyInstance().format(value);
+      return NumberFormat.getCurrencyInstance(Locale.GERMANY).format(value);
     }
     return null;
   }
