f200374f797ade3d6a330036555168506b1ff428
Manuel Pache <manuel.pache@europace.de>
2022-03-25 20:11:38 +0100

    [MPA] redundant repository methods removed

--- src/main/java/banking/account/service/repository/AccountRepository.java
+++ src/main/java/banking/account/service/repository/AccountRepository.java
@@ -1,17 +1,10 @@
 package banking.account.service.repository;
 
 import banking.account.service.domain.Account;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-import java.util.List;
 
 public interface AccountRepository extends JpaRepository<Account, Long>, JpaSpecificationExecutor<Account> {
 
   Account findAccountByIban(String iban);
-
-  List<Account> findAll();
-
-  List<Account> findAll(Specification specification);
 }
